4cd70dc8f9cf421ee2eb3c66f8e8d5503a270a55
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2024-10-19 03:00:10 +0700

    Add logging system

--- src/utils/logging_config.py
+++ src/utils/logging_config.py
@@ -1,17 +1,21 @@
-
+import sys
 from pathlib import Path
 from loguru import logger
 
-# Remove logging in terminal
+# Remove default logger to avoid clutter in the console
 logger.remove()
 
+# Create log directory if it doesn't exist
 log_dir = Path('logs')
 log_dir.mkdir(parents=True, exist_ok=True)
+
+# Add new logger for stdout (console output) with 'INFO' and above
+logger.add(sys.stdout, level="INFO")
 
 def filter_success(record):
     return "Scraping" in record["message"] and record["level"].name == "SUCCESS"
 
-logger.add(log_dir/ "scraping_{time}.log", retention='30 days', level='INFO')
+logger.add(log_dir/ "scraping.log", retention='30 days', level='INFO')
 logger.add(log_dir/  "success.log", retention='30 days', level='SUCCESS', filter=filter_success)
 logger.add(log_dir/ "error.log", retention='30 days', level='ERROR')
 logger.add(log_dir/ "warning.log", retention='30 days', level='WARNING')
