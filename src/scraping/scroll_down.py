22b38cf76b5ab18ee764ab8988e098a9c580d119
Wittaya Tangdorn <83308832+DeliciousBoy@users.noreply.github.com>
2024-10-19 13:41:12 +0700

    Update scroll_down function
    Change method to get full load on product list

--- src/scraping/scroll_down.py
+++ src/scraping/scroll_down.py
@@ -6,23 +6,42 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
-def scroll_down(driver):
-    last_height = driver.execute_script("return document.body.scrollHeight")
+product_list = "ProductListing_productItemWrapper__1AXaV"
 
-    while True:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        
+def scroll_down(driver, max_scroll_attempts=5, scroll_increment=500 ,sleep_time=2, scale_factor=0.5):
+    # Decrease a window scale
+    driver.execute_script(f"document.body.style.transform='scale({scale_factor})'; document.body.style.transformOrigin='0 0';")
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    scroll_attempt = 0
+    
+    previous_product_count = len(driver.find_elements(By.CLASS_NAME, product_list))
+    
+    while scroll_attempt < max_scroll_attempts:
+        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        for scroll_position in range(0, last_height, scroll_increment):
+            driver.execute_script(f"window.scrollTo(0, {scroll_position});")
+            time.sleep(sleep_time)
+            
         try:
             WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.CSS_SELECTOR, "div:last-child"))
+                EC.presence_of_element_located((By.CLASS_NAME, product_list))
             )
         except TimeoutException:
             # logger.warning("Timeout waiting for products to load")
-            pass
+            scroll_attempt += 1
+            continue
         
-        time.sleep(2)  
-
+        time.sleep(sleep_time)  
+        
+        current_product_count = len(driver.find_elements(By.CLASS_NAME, product_list))
         new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break  
+        
+        if new_height  == last_height and current_product_count == previous_product_count:
+            break
+        
         last_height = new_height
+        previous_product_count = current_product_count
+        scroll_attempt = 0
+    
+        
